d51a1c0acf2bbbc03561c075cf40ffc62bfe8f84
wengkhing <wengkhing@gmail.com>
2014-11-20 22:57:44 +0800

    age validation commented

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ActiveRecord::Base
 validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 validates :phone, format: { with: /\(\d{3}\) / on: :create}
 validates :email, uniqueness: true
-validates :age numericality: {greater_than_equal_to: 5}
+# validates :age numericality: {greater_than_equal_to: 5}
 # validates :birthday, numericality: 
 	def name
 		"#{first_name} #{last_name}"
