a7751ead576b499567219f842f4bc874b01efe1a
wengkhing <wengkhing@gmail.com>
2014-11-20 15:42:02 +0800

    Student#name, Student#age, validation
    Implemented Student#name, Student#age and basic validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+validates :email, uniqueness: true
+validates :birthday, numericality: 
+	def name
+		"#{first_name} #{last_name}"
+	end
+
+	def age
+		Date.today.year - birthday.year
+	end
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,9 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+    	t.string :first_name, :last_name, :gender, :email, :phone
+    	t.date :birthday
+    end
   end
 end
